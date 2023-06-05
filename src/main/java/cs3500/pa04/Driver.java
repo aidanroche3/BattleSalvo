6fafb0e2b7a2ee11a72e2a9402d7b43675aab4cd
Aidan Roche <aidanroche937@gmail.com>
2023-06-05 15:52:36 -0400

    Adding PA03 Contents

--- src/main/java/cs3500/pa04/Driver.java
+++ src/main/java/cs3500/pa04/Driver.java
@@ -1,6 +1,28 @@
 package cs3500.pa04;
 
-// TODO: document
+import cs3500.pa03.controller.BattleSalvoController;
+import cs3500.pa03.model.ComputerPlayer;
+import cs3500.pa03.model.ConsolePlayer;
+import cs3500.pa03.model.ConsolePlayerDependencies;
+import cs3500.pa03.view.BattleSalvoConsoleView;
+import cs3500.pa03.view.BattleSalvoView;
+import java.io.InputStreamReader;
+import java.util.Random;
+
+/**
+ * This is the main driver of this project.
+ */
 public class Driver {
-  // TODO: implement
+  /**
+   * Project entry point
+   *
+   * @param args - no command line args required
+   */
+  public static void main(String[] args) {
+    BattleSalvoView view = new BattleSalvoConsoleView(new InputStreamReader(System.in), System.out);
+    ConsolePlayer playerOne = new ConsolePlayer("User",
+        new Random(), new ConsolePlayerDependencies());
+    ComputerPlayer playerTwo = new ComputerPlayer(new Random());
+    new BattleSalvoController(view, playerOne, playerTwo).run();
+  }
 }
