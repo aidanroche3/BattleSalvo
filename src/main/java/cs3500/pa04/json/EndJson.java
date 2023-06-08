022fcffb99e514b959e007bd2ee7a46be164167f
Aidan Roche <aidanroche937@gmail.com>
2023-06-08 17:57:05 -0400

    Added extra record and enums.

---
+++ src/main/java/cs3500/pa04/json/EndJson.java
@@ -0,0 +1,9 @@
+package cs3500.pa04.json;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import cs3500.pa03.model.GameResult;
+
+public record EndJson(
+    @JsonProperty("result") GameResult result,
+    @JsonProperty("reason") String reason) {
+}
